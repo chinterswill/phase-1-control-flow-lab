7397d10209b85c7f58c2a2c260973d044636b907
Kamran Hussein <chinterswill@gmail.com>
2022-10-31 16:16:10 -0400

    commit tree 1f3c1d0979c7431a99c5fc7e59d5010b37789f08

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someNumber){
   // Write your code here!
+  let result 
+  if (someNumber <= 400) {
+    result = "This one is on me!";
+  }
+  else if (someNumber > 400 && someNumber < 2000) {
+    result = "That will be twenty bucks.";
+  }
+  else if (someNumber > 2000 && someNumber < 2500) {
+    result = "I will gladly take your thirty bucks.";
+  }
+  else if (someNumber > 2500) {
+    result = "No can do.";
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go."
+    
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+
 }
